09368e9f223154bd08b546a8f4c66bb1769ea9e5
bartstachurski <bart.stachurski@gmail.com>
2019-07-05 14:47:22 -0400

    adds validations for actor and movie models still needs happy and sad paths for update and create actions

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,10 @@
 class Movie < ApplicationRecord
+  validates :year, presence: true
+  validates :year, numericality: { greater_than: 1878 }
+  validates :title, presence: true
+  validates :title, length: { maximum: 255 }
+  validates :title, uniqueness: true
+  validates :director, presence: true
+  validates :plot, presence: true
+  validates :plot, length: { minimum: 10 }
 end
